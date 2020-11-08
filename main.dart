11f1ae6177e1fa823806d22f69a4a8d41d2ae1cf
KaranSalunkhe27 <74097977+KaranSalunkhe27@users.noreply.github.com>
2020-11-08 21:30:38 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,72 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:math';
-void main()=>runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyClass(),
-    );
-  }
-}
-class MyClass extends StatefulWidget {
-  @override
-  _MyClassState createState() => _MyClassState();
-}
-
-class _MyClassState extends State<MyClass> {
-
-  int Number = 0;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.teal,
-        title: Text("Counting App",style: TextStyle(fontWeight: FontWeight.bold),),
-      ),
-      body: Container(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(child: Text("Press Button to Change Number",style: TextStyle(fontSize: 20.0),)),
-            SizedBox(height: 20.0,),
-            Text(Number.toString(),style: TextStyle(fontWeight:FontWeight.bold,fontSize: 40.0),),
-            SizedBox(height: 20.0,),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RoundIconButton(
-                  onPressed: (){
-                  setState(() {
-                   Number= Random().nextInt(100);
-                  });
-                },
-                  icon:FontAwesomeIcons.plus,
-                ),
-              ],
-            ),
-
-          ],
+import 'package:flutter/material.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:Center(
+            child: Text("Dicee"),
+          ),
+          backgroundColor: Colors.blueGrey,
         ),
+        backgroundColor: Colors.blue,
+        body:DicePage(),
+      ),
+    ),
+  );
+}
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    int leftDiceNumber = Random().nextInt(6)+1;
+    int rightDiceNumber = Random().nextInt(6)+1 ;
+    void changeDiceNumber(){
+      setState(() {
+        leftDiceNumber = Random().nextInt(6)+1;
+        rightDiceNumber = Random().nextInt(6)+1;
+      });
+    }
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child:FlatButton(
+            onPressed: (){
+              changeDiceNumber();
+            },
+            child: Image(
+              image: AssetImage('images/dice$leftDiceNumber.jpg'),
+            ),
+          )
+          ),
+          Expanded(child:FlatButton(
+            onPressed: (){
+              changeDiceNumber();
+            },
+            child: Image(
+              image: AssetImage('images/dice$rightDiceNumber.jpg'),
+            ),
+          )
+          ),
+        ],
       ),
     );
   }
 }
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({this.onPressed,this.icon});
-  final Function onPressed;
-  final IconData icon;
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-        onPressed:onPressed,
-    constraints: BoxConstraints.tightFor(
-      width: 56.0,
-      height: 56.0,
-    ),
-      shape: CircleBorder(),
-      fillColor: Colors.teal,
-    );
-  }
-}
+
+
